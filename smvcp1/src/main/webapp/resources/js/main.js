6b10e687c9e45791bbec831d15a4b4dc92b48727
Pep Mendez <jmendez1@xtec.cat>
2015-10-16 21:12:19 +0200

    Added modal dialogs for help and about menus

---
+++ smvcp1/src/main/webapp/resources/js/main.js
@@ -0,0 +1,69 @@
+$(document).ready(function() {
+
+});
+
+$(function() {
+	$("#about_dialog").dialog({
+		autoOpen : false,
+		modal : true,
+		width : 650,
+		height : 350,
+		resize : true,
+		show : {
+			effect : "blind",
+			delay : 100,
+			duration : 1000
+		},
+		hide : {
+			/* fade */
+			effect : "explode",
+			duration : 1000
+		}
+	});
+
+	$("#about_opener").click(function() {
+		$("#about_dialog").dialog("open");
+	});
+});
+
+$(function() {
+	$("#help_dialog").dialog({
+		autoOpen : false,
+		modal : true,
+		width : 650,
+		height : 350,
+		resize : true,
+		show : {
+			effect : "blind",
+			delay : 100,
+			duration : 1000
+		},
+		hide : {
+			/* fade */
+			effect : "explode",
+			duration : 1000
+		}
+	});
+
+	$("#help_opener").click(function() {
+		$("#help_dialog").dialog("open");
+	});
+});
+
+$(".confirmLink").click(function(e) {
+    e.preventDefault();
+    var targetUrl = $(this).attr("href");
+
+    $("#dialog").dialog({
+      buttons : {
+        "Confirm" : function() {
+          window.location.href = targetUrl;
+        },
+        "Cancel" : function() {
+          $(this).dialog("close");
+        }
+      }
+    });
+
+    $("#dialog").dialog("open");
+  });
