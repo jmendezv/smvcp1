bb66a9dae739c37fa64b1311322af013fa27ca6a
Pep Mendez <jmendez1@xtec.cat>
2015-10-24 11:48:58 +0200

    Minor changes in properties files

--- smvcp1/src/test/java/pep/mendez/smvcp1/BCryptTest.java
+++ smvcp1/src/test/java/pep/mendez/smvcp1/BCryptTest.java
@@ -5,12 +5,18 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+/*
+ * insert into users values (null, 1, '$2a$10$k/u79QN2iQzzK4I88MgrK..NAj7N2u5pbo942Dz5cAZIy3TnpuyOG', 'pep@smvcp.com');
+ * insert into authorities values (null, 'ROLE_USER', 'pep@smvcp.com', 1);
+ * insert into authorities values (null, 'ROLE_ADMIN', 'pep@smvcp.com', 1);
+ */
+
 public class BCryptTest {
 
 	@Test
 	public void test() {
 		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-		String s = encoder.encode("pass5");
+		String s = encoder.encode("123456");
 		System.out.println(s);
 	}
 
